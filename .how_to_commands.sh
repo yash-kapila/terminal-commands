7384c7d85e02884d2a4d876d851e644ef9940fda
Yash Kapila <yash@backbase.com>
2019-04-01 13:05:01 +0100

    added function for github remote sync

--- .how_to_commands.sh
+++ .how_to_commands.sh
@@ -100,10 +100,15 @@ function __kubernetes_info() {
   echo
 }
 
+function __sync_with_github_remote() {
+  echo "Syncing with GitHub master"
+  git pull origin master
+}
+
 function howto() {
   echo "Enter the index:"
 
-  OPTIONS=("git" "unix" "ibmcloud" "docker" "kubernetes")
+  OPTIONS=("git" "unix" "ibmcloud" "docker" "kubernetes" "Github Sync")
 
   select option in "${OPTIONS[@]}";
   do
@@ -122,6 +127,9 @@ function howto() {
         ;;
       "kubernetes")
         __kubernetes_info
+        ;;
+      "Github Sync")
+        __sync_with_github_remote
         ;;
       *)
         echo "Err.. Incorrect option selected"
